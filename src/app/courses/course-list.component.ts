34534832a80ae82b41cc0f99df49568bd7aca30b
Claudioniz Pereira <claudioniz@gmail.com>
2021-01-30 12:26:34 -0300

    Injeção de dependência courseService

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -8,28 +9,9 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit {
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService){}
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-123',
-                duration: 120,
-                rating: 5,
-                releaseDate: 'Janeiro, 2, 2020'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 150.10,
-                code: 'NET-01',
-                duration: 180,
-                rating: 4,
-                releaseDate : 'Maio, 12, 2020'
-            }
-        ]
+        this.courses= this.courseService.retrieveAll();    
     }
 }
