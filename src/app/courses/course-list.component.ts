c167db245496be69d00848f7761fd13f3b0ff277
Claudioniz Pereira <claudioniz@gmail.com>
2021-01-30 11:13:59 -0300

    Inclusão course list.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -12,22 +12,22 @@ export class CourseListComponent implements OnInit {
         this.courses = [
             {
                 id: 1,
-                name: 'Angular Forms',
-                imageUrl: '',
+                name: 'Angular: Forms',
+                imageUrl: '/assets/images/forms.png',
                 price: 99.99,
                 code: 'XPS-123',
                 duration: 120,
-                rating: 5.4,
+                rating: 5,
                 releaseDate: 'Janeiro, 2, 2020'
             },
             {
                 id: 2,
-                name: 'ASP.NET',
-                imageUrl: '',
+                name: 'Angular: HTTP',
+                imageUrl: '/assets/images/http.png',
                 price: 150.10,
                 code: 'NET-01',
                 duration: 180,
-                rating: 7.8,
+                rating: 4,
                 releaseDate : 'Maio, 12, 2020'
             }
         ]
